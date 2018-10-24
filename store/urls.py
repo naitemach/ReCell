b1f6380c63710f010381ac596acadd88fb4f7425
silentwitch <arpith@Arpiths-MacBook-Pro.local>
2018-10-24 15:41:16 +0530

    Basic tree ready

--- store/urls.py
+++ store/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('profile',views.profile, name = 'profile'),
+    path('home',views.home,name = 'home'),
+    path('search',views.search,name='search')
 ]
