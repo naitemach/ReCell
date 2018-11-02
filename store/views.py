ecf522d9ea74a2ecd55b3561b5cedb49841d1a5a
NachiketUN <nachiketun8@gmail.com>
2018-11-02 23:35:33 +0530

    Registration Form Finally Works YAAAAAAAAAAAAAAAAAY

--- store/views.py
+++ store/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from .forms import LoginForm, RegisterForm
-from .models import Item, ItemDesc, User
+from .models import User, Location, Wallet
 from django.db import connection
 
 
@@ -56,33 +56,50 @@ def register(request):
         # check whether it's valid:
         if form.is_valid():
             email = form.cleaned_data['email']
-            user_obj = User.objects.filter(email=form.cleaned_data['email'], password=form.cleaned_data['password'])
+            user_obj = User.objects.filter(email=form.cleaned_data['email'])
             if user_obj.exists():
                 error = "Account with given Email ID already exists"
                 return render(request, 'store/register.html', {'error': error})
             first_name = form.cleaned_data['first_name']
             email = form.cleaned_data['email']
-            obj = User.objects.create()
+            loci = Location.objects.create()
+            loci = Location.objects.create()
+            loci.city_name = form.cleaned_data['city']
+            loci.zip_code = form.cleaned_data['zip_code']
+            loci.address = form.cleaned_data['address']
+            loci.save()
+
+            wallet = Wallet.objects.create(credits=10000)
+
+            wallet.save()
+
+            obj = User.objects.create(is_seller=False, loc=loci, wall=wallet)
             obj.email = email
             obj.first_name = form.cleaned_data['first_name']
             obj.last_name = form.cleaned_data['last_name']
             obj.password = form.cleaned_data['password']
+            obj.dob = form.cleaned_data['dob']
+            if form.cleaned_data['seller'] == "1":
+                obj.is_seller = True
+            else:
+                obj.is_seller = False
 
-            test = form.cleaned_data
+            obj.save()
+
             request.session['email'] = email
-            request.session['first_name'] = user_obj.get().first_name
-            request.session['id'] = user_obj.get().u_id
-            return render(request, 'store/test.html', test)
-            #return render(request, 'store/index.html', {'username': request.session['username']})
+            request.session['first_name'] = first_name
+            request.session['id'] = id
+            return render(request, 'store/test.html', {'test':obj.first_name})
+            # return render(request, 'store/index.html', {'username': request.session['username']})
 
         return render(request, 'store/form.html', {'form': form, })
 
     # if a GET (or any other method) we'll create a blank form
     else:
-        form = LoginForm()
+        form = RegisterForm()
 
-    #return render(request, 'store/login.html', {'form': form})
-    return render(request, 'store/register.html', {})
+    # return render(request, 'store/login.html', {'form': form})
+    return render(request, 'store/register.html', {'form':form})
 
 
 def search(request):
@@ -98,6 +115,7 @@ def display(request):
     itemdesc = cursor.fetchone()
 
     return render(request, 'store/display.html', {'item': item, 'itemdesc': itemdesc})
+
 
 def productReg(request):
     return render(request, 'store/product_reg.html', {})
