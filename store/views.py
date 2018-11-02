c4dd0389e5907db8981791149866f738607a9fe8
naitemach <npsnayak@gmail.com>
2018-11-02 14:02:20 +0530

    added product_details.html

--- store/views.py
+++ store/views.py
@@ -25,7 +25,7 @@ def login(request):
 
 
 def index(request):
-    return render(request, 'store/login.html', {})
+    return render(request, 'store/product_details.html', {})
 
 
 def profile(request):
