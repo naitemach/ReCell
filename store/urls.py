ef792c5151055d2ddb527d07b61342932447939c
silentwitch <arpith@Arpiths-MacBook-Pro.local>
2018-11-03 11:28:56 +0530

    Added items and credits display

--- store/urls.py
+++ store/urls.py
@@ -9,9 +9,9 @@ urlpatterns = [
     path('search', views.search, name='search'),
     path('login', views.login, name='login'),
     path('display', views.display, name='display'),
-    path('products', views.products, name='products'),
+    path('productDetails', views.productDetails, name='products'),
     path('register', views.register, name='register'),
     path('productReg', views.productReg, name='productReg'),
-
+    path('productSummary',views.productSummary,name='productSummary'),
     # static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 ]
