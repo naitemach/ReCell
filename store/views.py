203f49ca68d7b0ec195744e7ea8d60efe853695b
NachiketUN <nachiketun8@gmail.com>
2018-11-03 15:00:08 +0530

    Fix Views

--- store/views.py
+++ store/views.py
@@ -7,6 +7,7 @@ from django.db import connection
 
 
 def login(request):
+
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
@@ -17,7 +18,10 @@ def login(request):
                 request.session['first_name'] = user_obj.get().first_name
                 request.session['id'] = user_obj.get().u_id
                 request.session['credits'] = user_obj.get().wall.credits
-                return render(request, 'store/index.html', {'first_name': request.session['first_name']})
+                credits = request.session.get('credits')
+                items = request.session.get('items')
+                id = request.session.get('id')
+                return render(request, 'store/index.html', {'first_name': request.session['first_name'],'credits': credits, 'items': items, 'id':id})
 
             return render(request, 'store/form.html', {'form': form, 'email': email})
     else:
@@ -29,8 +33,9 @@ def index(request):
     fname = request.session.get('first_name')
     credits = request.session.get('credits')
     items = request.session.get('items')
+    id = request.session.get('id')
     if fname != None and credits != None:
-        return render(request, 'store/index.html', {'first_name': fname, 'credits': credits, 'items': items})
+        return render(request, 'store/index.html', {'first_name': fname, 'credits': credits, 'items': items, 'id':id})
     else:
         return HttpResponse("Fname couldnt be passes succesfully")
 
@@ -143,20 +148,8 @@ def productSummary(request):
 
 
 def productReg(request):
-    # if request.method == 'POST':
-    #     form = ProdRegistration(request.POST)
-    #     return HttpResponse({form})
-    #     print (form.is_valid())
-    #     if form.is_valid():
-    #         test = form.cleaned_data['product_name']
-    #         return render(request, 'store/test.html', {'test':test})
-    # form=ProdRegistration()
-    # return render(request, 'store/product_reg.html', {'form':form})
-    # if this is a POST request we need to process the form data
     if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
         form = ProdRegistration(request.POST)
-        # check whether it's valid:
         if form.is_valid():
             desc = ItemDesc.objects.create(age=form.cleaned_data['age'], name=form.cleaned_data['product_name'],
                                            comments=form.cleaned_data['additional_information'])
